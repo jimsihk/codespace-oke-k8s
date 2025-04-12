5b4caa73377e3354a221e9f2f127b2f1e4cd516d
jimsihk <99048231+jimsihk@users.noreply.github.com>
2025-04-13 02:11:04 +0800

    Change entrypoint and add build test on container run (#7)

---
+++ test/run_tests.sh
@@ -0,0 +1,36 @@
+#/bin/bash
+
+if [ -z "$1" ]; then
+  TEST_IMAGE='jimsihk/codespace-oke-k8s:test'
+else
+  TEST_IMAGE="$1"
+fi
+
+echo "Testing ${TEST_IMAGE}"
+
+echo "=====Test installed packages====="
+docker run --rm -v $(pwd)/test_container.sh:/mnt/test.sh "${TEST_IMAGE}" "cp /mnt/test.sh test.sh && chmod +x test.sh && ./test.sh; echo \$?" > /tmp/test_result.txt 2>&1
+cat /tmp/test_result.txt
+STATUS="$(cat /tmp/test_result.txt | tail -1)"
+if [ "${STATUS}" -eq 0 ]; then
+  echo "Passed"
+else
+  echo "Failed"
+  exit "${STATUS}"
+fi
+unset STATUS
+echo "....."
+
+echo "=====Test entrypoint====="
+CONTAINER_ID=$(docker run --rm -d "${TEST_IMAGE}")
+docker ps
+docker logs --details "${CONTAINER_ID}"
+echo "Wait for 10s..." && sleep 10
+docker ps
+docker logs --details "${CONTAINER_ID}"
+echo "Wait for 30s..." && sleep 30
+docker ps
+docker logs --details "${CONTAINER_ID}"
+echo "Kill the container..."
+docker kill "${CONTAINER_ID}"
+echo "....."
---
+++ test/test_container.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -eo pipefail
+
+echo "Current user: $(whoami)"
+echo "Current directory: $(pwd)"
+
+echo '****************'
+echo '~/.bashrc:'
+cat ~/.bashrc
+
+echo '****************'
+echo 'env:'
+env
+
+echo '****************'
+echo 'alias:'
+alias
+
+echo '****************'
+echo "Installed versions:"
+echo "- oci: $(oci --version)"
+echo "- kubectl:" && kubectl version --client
+echo "- helm: $(helm version)"
+echo "- git: $(git --version)"
+echo "- k9s:" && k9s version
+echo "- kdash: $(kdash --version)"
+
+echo '****************'
+echo "Testing oke-tunnel.sh:"
+oke-tunnel.sh && exit_code=0 || exit_code=$?
+echo "$exit_code"
+if [ "$exit_code" -eq 4 ]; then
+  echo "Passed"
+else
+  echo "Failed"
+  exit "$exit_code"
+fi
