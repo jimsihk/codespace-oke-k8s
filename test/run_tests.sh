66ae38de9864e610c4b8f92fafdb70097a7b8fbe
jimsihk <99048231+jimsihk@users.noreply.github.com>
2025-04-13 01:27:11 +0800

    Update run_tests.sh

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -8,11 +8,9 @@ fi
 
 echo "Testing ${TEST_IMAGE}"
 
-echo "Test installed packages..."
+echo "=====Test installed packages====="
 docker run --rm -v $(pwd)/test_container.sh:/mnt/test.sh "${TEST_IMAGE}" "cp /mnt/test.sh test.sh && chmod +x test.sh && ./test.sh; echo \$?" > /tmp/test_result.txt 2>&1
-echo "---"
 cat /tmp/test_result.txt
-echo "---"
 STATUS="$(cat /tmp/test_result.txt | tail -1)"
 if [ "${STATUS}" -eq 0 ]; then
   echo "Passed"
@@ -21,9 +19,14 @@ else
   exit "${STATUS}"
 fi
 unset STATUS
+echo "....."
 
-echo "Test entrypoint..."
+echo "=====Test entrypoint====="
 docker run --rm --name t_entrypt -d "${TEST_IMAGE}"
+docker ps
+sleep 10
+docker ps
 sleep 60
+docker ps
 docker logs --details t_entrypt
 docker kill t_entrypt
