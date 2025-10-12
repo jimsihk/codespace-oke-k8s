3e649fe8e71f9e48e45e47dcfc52e803d3cea07f
jimsihk <99048231+jimsihk@users.noreply.github.com>
2025-10-12 13:49:21 +0800

    Use python3 instead of python to list version

--- test/test_container.sh
+++ test/test_container.sh
@@ -14,7 +14,7 @@ echo "- helm: $(helm version)"
 echo "- git: $(git --version)"
 echo "- k9s:" && k9s version
 echo "- kdash: $(kdash --version)"
-echo "- python: $(python -version)"
+echo "- python: $(python3 -version)"
 
 echo '****************'
 echo "Testing oke-tunnel.sh:"
