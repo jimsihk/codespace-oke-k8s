bbcaa612fb1ece14438cba0d46b3cbcdff8ff1fe
jimsihk <99048231+jimsihk@users.noreply.github.com>
2025-11-09 03:21:59 +0000

    Update remarks

--- .devcontainer/okeutil/load-oke-config.sh
+++ .devcontainer/okeutil/load-oke-config.sh
@@ -7,13 +7,16 @@
 # OCI_CLI_CONFIG_FILE
 # OCI_CLI_REGION
 # OCI_CLI_USER
-# OCI_CLI_KEY_FILE
+# OCI_CLI_KEY_CONTENT
 # OCI_CLI_TENANCY
 # 
 # Custom variables:
+# OCI_CLI_PUBLIC_KEY_CONTENT
 # OCI_OKE_CLUSTER
 # OCI_OKE_PRIVATE_IP
 # OCI_BASTION
 ################################
 
 # TODO
+
+# If all variables exist, then run init-local-oci.sh
