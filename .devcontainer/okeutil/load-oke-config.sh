73d081a08a327feaf3abf65d8f36cb4d0fd2596d
jimsihk <99048231+jimsihk@users.noreply.github.com>
2025-11-09 03:16:54 +0000

    Update remarks

--- .devcontainer/okeutil/load-oke-config.sh
+++ .devcontainer/okeutil/load-oke-config.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
 ################################
-# Auto config oci local setup from environment variables
+# Auto config oci local setup from environment variables:
+#
+# Same as OCI CLI environment variables:
+# OCI_CLI_CONFIG_FILE
+# OCI_CLI_REGION
+# OCI_CLI_USER
+# OCI_CLI_KEY_FILE
+# OCI_CLI_TENANCY
+# 
+# Custom variables:
+# OCI_OKE_CLUSTER
+# OCI_OKE_PRIVATE_IP
+# OCI_BASTION
 ################################
 
 # TODO
