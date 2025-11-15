49a16919a49ed0e25133f7434a005262f0d3593a
jimsihk <99048231+jimsihk@users.noreply.github.com>
2025-11-16 02:02:34 +0800

    Refactor uv hack scripts

---
+++ .devcontainer/hack/patch-oci-install-py.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -n "$1" ]
+then
+  INSTALL_SCRIPT="$1"
+else
+  INSTALL_SCRIPT="./install.py"
+fi
+
+if [ -f "$INSTALL_SCRIPT" ]
+then
+  sed -i 's/upgrade_pip_wheel/#upgrade_pip_wheel/g' "$INSTALL_SCRIPT"
+  sed -i 's/def #upgrade_pip_wheel/def upgrade_pip_wheel/g' "$INSTALL_SCRIPT"
+  sed -i "s|path_to_pip = os.path.join(install_dir, 'bin', 'pip')|path_to_pip = os.path.join('/usr/local/lib/hack', 'uv-pip')|g" "$INSTALL_SCRIPT"
+else
+  echo "Error! install.py not found!"
+  exit 2
+fi
