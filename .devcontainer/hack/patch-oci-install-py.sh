b09d20735da8db2ad948a74c29ebb9095bc9715d
jimsihk <99048231+jimsihk@users.noreply.github.com>
2025-12-25 01:52:32 +0800

    Add debug log

--- .devcontainer/hack/patch-oci-install-py.sh
+++ .devcontainer/hack/patch-oci-install-py.sh
@@ -9,6 +9,7 @@ fi
 
 if [ -f "$INSTALL_SCRIPT" ]
 then
+  echo "Patching ${INSTALL_SCRIPT} for oci-cli installation"
   sed -i 's/upgrade_pip_wheel/#upgrade_pip_wheel/g' "$INSTALL_SCRIPT"
   sed -i 's/def #upgrade_pip_wheel/def upgrade_pip_wheel/g' "$INSTALL_SCRIPT"
   sed -i "s|path_to_pip = os.path.join(install_dir, 'bin', 'pip')|path_to_pip = os.path.join('/usr/local/lib/hack', 'uv-pip')|g" "$INSTALL_SCRIPT"
